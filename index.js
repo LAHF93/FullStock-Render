c55f62a7073c1f9ea95141d79f34648ca313012c
Luiggi Abello-Hurtado Fuchs <lahf@Luiggis-MacBook-Air.local>
2025-09-17 22:38:15 -0500

    uploading views files.ejs

--- index.js
+++ index.js
@@ -11,6 +11,34 @@ app.get("/", (_req, res) => {
   res.render("index");
 });
 
+app.get("/signup", (_req, res) => {
+  res.render("signup");
+});
+
+app.get("/login", (_req, res) => {
+  res.render("login");
+});
+
+app.get("/categories", (_req, res) => {
+  res.render("categories");
+});
+
+app.get("/product", (_req, res) => {
+  res.render("product");
+});
+
+app.get("/cart", (_req, res) => {
+  res.render("cart");
+});
+
+app.get("/checkout", (_req, res) => {
+  res.render("checkout");
+});
+
+app.get("/thanks", (_req, res) => {
+  res.render("thanks");
+});
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://locaslhost:${port}`);
 });
