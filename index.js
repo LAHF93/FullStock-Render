22a3f01b6b59ce2579212320c79a0843641ceb01
Luiggi Abello-Hurtado Fuchs <lahf@Luiggis-MacBook-Air.local>
2025-09-19 18:47:03 -0500

    uploading changes in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import expressEjsLayouts from "express-ejs-layouts";
+import fs from "node:fs/promises";
 
 const port = 5500;
 const app = express();
@@ -8,20 +9,36 @@ app.use(express.static("./public"));
 app.use(expressEjsLayouts);
 app.set("layout", "layout");
 
-app.get("/", (_req, res) => {
-  res.render("index", { title: "FullStock" });
+async function getCategories() {
+  const categories = await fs.readFile("./data.json", "utf8");
+  return JSON.parse(categories);
+}
+
+async function getProducts(categoryId) {
+  const content = await fs.readFile("./data.json", "utf8");
+  const categories = JSON.parse(content);
+  const category = categories.find((c) => c.id === categoryId);
+  return category.products;
+}
+
+app.get("/", async (_req, res) => {
+  const categories = await getCategories();
+  res.render("home", { categories, title: "FullStock | Home" });
 });
 
+app.get("/categories/:id", async (req, res) => {
+  const id = +req.params.id;
+  const products = await getProducts(id);
+  res.render("categories", { products, title: "Fullstock | Categories" });
+});
+
+/*
 app.get("/signup", (_req, res) => {
   res.render("signup", { title: "SignUp" });
 });
 
 app.get("/login", (_req, res) => {
   res.render("login", { title: "Login" });
-});
-
-app.get("/categories", (_req, res) => {
-  res.render("categories", { title: "Categories" });
 });
 
 app.get("/product", (_req, res) => {
@@ -39,6 +56,7 @@ app.get("/checkout", (_req, res) => {
 app.get("/thanks", (_req, res) => {
   res.render("thanks", { title: "Thanks" });
 });
+*/
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://locaslhost:${port}`);
